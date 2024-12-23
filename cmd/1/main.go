caf161e84172f1268143f9ecb6f917d04aa5aa03
James <evilrobotjames@gmail.com>
2024-12-23 23:03:53 +0000

    Day 1, part 2.

--- cmd/1/main.go
+++ cmd/1/main.go
@@ -7,12 +7,11 @@ import (
     "sort"
 )
 
-func main() {
-
+func read() (list_1 []int, list_2 []int) {
     readFile, err := os.Open("input.txt")
 
-    list_1 := make([]int, 0)
-    list_2 := make([]int, 0)
+    list_1 = make([]int, 0)
+    list_2 = make([]int, 0)
 
     if err != nil {
         fmt.Println(err)
@@ -32,14 +31,13 @@ func main() {
     }
 
     readFile.Close()
+    return
+}
 
-    fmt.Println(list_1)
-    fmt.Println(list_2)
+func first(list_1 []int, list_2 []int) {
 
     sort.Sort(sort.IntSlice(list_1))
     sort.Sort(sort.IntSlice(list_2))
-    fmt.Println(list_1)
-    fmt.Println(list_2)
 
     var total_distance int
     total_distance = 0
@@ -48,9 +46,31 @@ func main() {
         var v2 = list_2[i]
         var distance = v1 - v2
         var abs_distance = max(distance, -distance)
-        fmt.Printf("%d %d = %d\n", v1, v2, abs_distance)
         total_distance = total_distance + abs_distance
     }
 
     fmt.Printf("total distance: %d\n", total_distance)
+}
+
+func second(list_1 []int, list_2 []int) {
+
+    similarity := 0
+    for _, v1 := range list_1 {
+        occurrances := 0
+        for _, v2 := range list_2 {
+            if v1 == v2 {
+                occurrances = occurrances + 1
+            }
+        }
+        similarity = similarity + (occurrances * v1)
+    }
+    fmt.Printf("similarity: %d\n", similarity)
+}
+
+func main() {
+    list_1, list_2 := read()
+    first(list_1, list_2)
+
+    list_1, list_2 = read()
+    second(list_1, list_2)
 }
